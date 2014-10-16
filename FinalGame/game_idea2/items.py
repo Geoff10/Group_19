16d2a28a7a0f623f52705cb9d830ddb01e8ad786
Geoff10 <jukesg@cardiff.cf.uk>
2014-10-16 15:45:03 +0100

    Added FinalGame files

---
+++ FinalGame/game_idea2/items.py
@@ -0,0 +1,42 @@
+item_battery = {
+    "id": "battery",
+
+    "name": "the battery",
+
+    "description":
+    """A small battery, could help me turn the lights on in this place."""
+}
+
+item_key = {
+    "id": "key",
+
+    "name": "key",
+
+    "description":
+    "A beaten key, i wonder if it could open a locked door"
+}
+
+item_pick = {
+    "id": "pick",
+
+    "name": "pick",
+
+    "description":
+    "Looks like a pick, this could be use to open locked doors"
+}
+
+item_money = {
+    "id": "money",
+
+    "name": "money",
+
+    "description":
+    """ Seems kinda useless for this place, maybe i should drop it. """
+}
+
+items = {
+    "battery" : item_battery,
+    "key" : item_key,
+    "pick" : item_pick,
+    "money": item_money
+}
---
+++ FinalGame/game_idea2/player.py
@@ -0,0 +1,9 @@
+from items import *
+from map import rooms
+
+inventory = [item_money]
+
+# Start game at the reception
+current_room = rooms["Hall"]
+
+fear = 0
